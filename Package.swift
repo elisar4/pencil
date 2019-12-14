5684eafdef4fcfddcf9d7d37cf29681cf7d12f2b
Vladimir <bobah1991@gmail.com>
2019-12-14 16:04:17 +0300

    spm support; swift version set to 5+

--- Package.swift
+++ Package.swift
@@ -1,5 +1,30 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
-    name: "Pencil"
+    name: "Pencil",
+    products: [
+        .library(
+            name: "Pencil",
+            targets: ["Pencil"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "Pencil",
+            dependencies: [],
+            path: ".",
+            exclude: [
+                "Example",
+                "Pencil",
+                "PencilTests",
+                "Cartfile.private",
+                "Cartfile.resilved",
+                "pencil.png",
+                "pencil.podspec"
+            ]
+        )
+    ]
 )
